554f8db1fe9beaf93036ce02ee3f37e947c618db
Justin <ngjustin2002@gmail.com>
2022-08-22 12:03:12 -0700

    updated api fetch

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,21 @@ import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
 import { useEffect, useState } from 'react'
 
-const dev = process.env.NODE_ENV !== 'production'; 
-const port = process.env.PORT || 3000; 
+// const dev = process.env.NODE_ENV !== 'production'; 
+// const port = process.env.PORT || 3000; 
 
 
 const Website = ({ Component, pageProps, router }) => {
-    if (dev) {
-        var url = `http://localhost:${port}`;
-    } else {
-        var url = `ngjstn-website.herokuapp.com`;
-    }
+    // if (dev) {
+    //     var url = `http://localhost:${port}`;
+    // } else {
+    //     var url = `ngjstn-website.herokuapp.com`;
+    // }
 
     const [backendData, setBackendData] = useState([{}])
 
     useEffect(() => {
-        fetch(`${url}/api`).then(
+        fetch('/api').then(
             response => response.json()
         ).then(
             data => {
