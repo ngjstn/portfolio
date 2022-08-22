adbcc6d99ed81228d4425542152ac2d5aca7c84e
Justin <ngjustin2002@gmail.com>
2022-08-22 16:00:21 -0700

    removed ssl force in express server

--- server.js
+++ server.js
@@ -33,19 +33,19 @@ if (!dev && cluster.isMaster) {
     .then(() => {
       const server = express();
 
-      if (!dev) {
-        // Enforce SSL & HSTS in production
-        server.use(function(req, res, next) {
-          var proto = req.headers["x-forwarded-proto"];
-          if (proto === "https") {
-            res.set({
-              'Strict-Transport-Security': 'max-age=31557600' // one-year
-            });
-            return next();
-          }
-          res.redirect("https://" + req.headers.host + req.url);
-        });
-      }
+      // if (!dev) {
+      //   // Enforce SSL & HSTS in production
+      //   server.use(function(req, res, next) {
+      //     var proto = req.headers["x-forwarded-proto"];
+      //     if (proto === "https") {
+      //       res.set({
+      //         'Strict-Transport-Security': 'max-age=31557600' // one-year
+      //       });
+      //       return next();
+      //     }
+      //     res.redirect("https://" + req.headers.host + req.url);
+      //   });
+      // }
       
       // Static files
       // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
